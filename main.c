4620672f2853e97443cfd60ce35c471415d39305
minkyeong417 <55428050+minkyeong417@users.noreply.github.com>
2019-10-15 15:57:17 +0900

    combination

--- main.c
+++ main.c
@@ -3,19 +3,41 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-	int square(int n);
-	
+	int fact(int x)
+	{
+		int i,plex;
+		
+		plex=x;
+		
+		for (i=x-1;i>0;i--)
+		{
+			plex=plex*i;
+		}
+		
+		return plex;
+	 } 
+
+	int comb(int a,int b)
+	{
+		int res;
+		res=fact(a)/(fact(b)*fact(a-b));
+		return res;
+		
+	}
+
 	int main(int argc,char*argv[]) 
 	
 	{
-		int result;
-		result=square(5);
-		printf("%d",result);
-	
+		int n,r,result;
+		
+		printf("input two integars:");
+		
+		scanf("%d%d",&n,&r );
+		
+		result=comb(n,r);
+		
+		printf("%d C %d = %d ",n,r,result);
+		
 		return 0;
 	}
 	
-	int square(int n)
-	{
-		return (n*n);
-	}
