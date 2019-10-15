dd73b68b11a476f23cdc56512b6a785cfa32345e
minkyeong417 <55428050+minkyeong417@users.noreply.github.com>
2019-10-15 15:25:15 +0900

    square

--- main.c
+++ main.c
@@ -3,38 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-	int sumTwo(int a,int b)
-	{
-		int output;
-		output=a+b;
-		return output;
-	}
-	
-	int square(int n)
-	{
-		return n*n;
-	}
-	
-	int get_max(int a,int b)
-	{
-		if (a>b) return a;
-		else return b;
-	}
-	
+	int square(int n);
 	
 	int main(int argc,char*argv[]) 
 	
 	{
-	int x;
-	int y;
-	x=5;
-	y=6;
+		int result;
+		result=square(5);
+		printf("%d",result);
 	
-	int result;
-	result=square(x);
+		return 0;
+	}
 	
-	printf("square: %d\n",result);
-	printf("sumTwo: %d\n",sumTwo(x,y));
-	
-	return 0;
+	int square(int n)
+	{
+		return (n*n);
 	}
